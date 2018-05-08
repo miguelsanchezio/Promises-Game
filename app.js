a3b5d8191b18f13b6033f437776bc2271048bd08
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-05-08 14:16:09 -0700

    Put all the game logic into a stand alone funciton.

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-let counter = 0;
+const startGame = () => {
+  let counter = 0;
 
-document.querySelector('button').addEventListener('click', () => {
-  ++counter;
-});
-
-setTimeout(() => {
-  if(counter > 5) {
-    alert(`You won. You clicked ${counter} times.`);
-  } else {
-    alert(`You lost. You clicked ${counter} times.`);
-  }
-}, 2000);
+  document.querySelector('button').addEventListener('click', () => {
+    ++counter;
+  });
+  
+  setTimeout(() => {
+    if(counter > 5) {
+      alert(`You won. You clicked ${counter} times.`);
+    } else {
+      alert(`You lost. You clicked ${counter} times.`);
+    }
+  }, 2000);
+}
